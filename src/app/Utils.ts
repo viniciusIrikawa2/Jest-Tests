c7e0526e4ef54f424fb56dabf54113c9cdc8049c
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-14 16:39:04 -0300

    tests: first tests

---
+++ src/app/Utils.ts
@@ -0,0 +1,21 @@
+export type StringInfo = {
+    lowerCase: string,
+    uppercase: string,
+    characters: string[],
+    length: number,
+    extraInfo: Object | undefined,
+}
+
+export const toUpperCase = (arg: string) => {
+    return arg.toUpperCase();
+};
+
+export const getStringInfo = (arg: string): StringInfo => {
+    return {
+        lowerCase: arg.toLowerCase(),
+        uppercase: arg.toUpperCase(),
+        characters: Array.from(arg),
+        length: arg.length,
+        extraInfo: {},
+    }
+};
---
+++ src/tests/Utils.spec.ts
@@ -0,0 +1,50 @@
+import { getStringInfo, toUpperCase } from "../app/Utils";
+
+describe('Utils test suite', () => {   
+
+    it('should return uppercase', () => {
+        // Arrange
+        const sut = toUpperCase;
+        const expected = 'ABC';
+
+        // Act
+        const actual = sut('abc');
+
+        // Assert
+        expect(actual).toBe(expected);
+
+    });
+
+    describe('getStringInfo for arg My-String should', () => {
+        test('return right length', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.length).toBe(9);
+            expect(actual.characters).toHaveLength(9);
+        });
+
+        test('return right lower case', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.lowerCase).toBe('my-string');  
+        });
+
+        test('return right upper case', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.uppercase).toBe('MY-STRING');
+        });
+
+        test('return extraInfo as empty object', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toEqual({});
+        });
+
+        test('return defined extraInfo', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toBeDefined();
+        });
+
+        test('return right characters', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+        });
+    });
+});
