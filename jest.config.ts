27f7897e37b15972dffcf3319bf3cfc13b85529c
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-15 19:36:11 -0300

    chore: code coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/app/**/*.ts'
+  ],
 }
 
 export default config;
--- src/tests/Utils.spec.ts
+++ src/tests/Utils.spec.ts
@@ -2,7 +2,7 @@ import { getStringInfo, StringUtils, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', () => {   
     
-    describe.only('StringUtils tests', () => {
+    describe('StringUtils tests', () => {
         
         let sut: StringUtils;
 
@@ -15,7 +15,7 @@ describe('Utils test suite', () => {
             console.log('Teardown');
         });
         
-        it.only('Should return correct upperCase', () => {
+        it('Should return correct upperCase', () => {
             const sut = new StringUtils();
             const actual = sut.toUpperCase('abc');
             expect(actual).toBe('ABC');
