8fd1e8900fdcd7372b5fde09a7f8b32341cb15bd
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-17 19:50:44 -0300

    tests: PasswordChecker iteration 1

--- src/tests/PasswordChecker/PasswordChecker.spec.ts
+++ src/tests/PasswordChecker/PasswordChecker.spec.ts
@@ -8,8 +8,34 @@ describe('PasswordChecker test suite', () => {
         sut = new PasswordChecker();
     });
 
-    it('Should do nothing for the moment', () => {
-        sut.checkPassword();
+    it('Password with less than 8 chars is invalid', () => {
+        const act = sut.checkPassword('12345');
+        expect(act).toBe(false);
+    });
+    
+    it('Password with more than 8 chars is valid', () => {
+        const act = sut.checkPassword('12345678Aa');
+        expect(act).toBe(true);
+    });
+
+    it('Password with no upper case letter is invalid', () => {
+        const act = sut.checkPassword('1234abcd');
+        expect(act).toBe(false);
+    });
+
+    it('Password with upper case letter is valid', () => {
+        const act = sut.checkPassword('1234abcdA');
+        expect(act).toBe(true);
+    });
+
+    it('Password with no lower case letter is invalid', () => {
+        const act = sut.checkPassword('1234ABCD');
+        expect(act).toBe(false);
+    });
+
+    it('Password with lower case letter is valid', () => {
+        const act = sut.checkPassword('1234ABCDa');
+        expect(act).toBe(true);
     });
 
 });
